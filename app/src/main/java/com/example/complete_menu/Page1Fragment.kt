2bd1c40c21cc6faa1e5d380276681f18c9c8466c
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-10-04 17:45:09 +0530

    Set up 3 dot menu

--- app/src/main/java/com/example/complete_menu/Page1Fragment.kt
+++ app/src/main/java/com/example/complete_menu/Page1Fragment.kt
@@ -1,11 +1,10 @@
 package com.example.complete_menu
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.complete_menu.databinding.FragmentPage1Binding
 
 class Page1Fragment : Fragment() {
@@ -17,14 +16,28 @@ class Page1Fragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         _binding = FragmentPage1Binding.inflate(inflater,container,false)
 
         binding.buttonToPage2.setOnClickListener {
             it.findNavController().navigate(R.id.action_page1Fragment_to_page2Fragment)
         }
-        
+
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.dot_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+                            requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {

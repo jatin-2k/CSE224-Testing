f837f1886316109c480fba585792d273fea43d04
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-11-02 12:39:16 +0530

    CA2 Q1

--- app/src/main/java/com/example/complete_menu/MainActivity.kt
+++ app/src/main/java/com/example/complete_menu/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.complete_menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.sign_in_button_id).setOnClickListener {
+            Snackbar.make(it, "done successfully!", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
